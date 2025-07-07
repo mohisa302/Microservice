1798d70939ff6ae161725d6a64c975ec97d4b0c5
mohisa56 <mohadese.sadeghi@hitaxi.cab>
2025-07-07 14:06:56 +0330

    Fix problem

--- Platform/Queries/GetAllPlatformsQuery.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-using PlatformService.Dtos;
-namespace PlatformService.Queries
-{
-  public class GetAllPlatformsQuery : IRequest<IEnumerable<PlatformReadDto>> //what query should eventually returns 
-  {
-    
-  }
-}
